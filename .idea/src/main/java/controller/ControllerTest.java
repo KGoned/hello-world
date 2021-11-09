b3f880422108e1f7c39157069c1b2a28b2ae9000
yanghucheng <54920296@qq.com>
2021-11-09 14:59:40 +0800

    更新1

---
+++ .idea/src/main/java/controller/ControllerTest.java
@@ -0,0 +1,13 @@
+package java.controller;
+
+/**
+ * @Description
+ * @Author： 杨虎成
+ * @Description
+ * @Date： Created in 14:52 2021/11/9
+ */
+public class ControllerTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
